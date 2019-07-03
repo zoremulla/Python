0895880fa8d6e2dbdc7edd89f58afa4a1f36db7f
MMulla <zore.mulla@gmail.com>
2019-07-03 20:55:49 +0300

    made changes to loop task

--- loop_task.py
+++ loop_task.py
@@ -5,8 +5,8 @@ while item_name!="done":    #while True:   this is another method to do
     item_name=input("item (enter \"done\" when finished):")
     if item_name="done"
         break
-    price= input("price:")
-    quantity=input("quantity: ")
+    price= float(input("price:"))
+    quantity=float(input("quantity: "))
 
     item={
         "name": item_name,
@@ -18,8 +18,9 @@ while item_name!="done":    #while True:   this is another method to do
 print("------------------------------------")
 print("Receipt")
 print("------------------------------------")
+total=0
 for item in list_of_items:
     print("%s %s %sKD"%(item["quantity"],item["name"],(item["quantity"]*item["price"])))
     total=total+item["quantity"]+item["price"]
-    print("--------------------------------")
-    print("Total: %s KD"% total)
+print("--------------------------------")
+print("Total: %s KD"% total)
